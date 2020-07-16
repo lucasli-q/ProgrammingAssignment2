58d5d7a37790eaefd552005b73e3f289391d17f1
lucasli-q <quim.lucasaug@gmail.com>
2020-07-16 20:52:40 -0300

    Defined functions, makeCacheMatrix and cacheSolve, to calculate the inverse of matrices not yet calculated in the environment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following function creates a special matrix from a list that defines functions
+## to set the values for the matrix and get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) inv <<- solve
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## The next function checks if the inverse of that matrix was already calculated
+## in the environment of the function. If not, it calculates the inverse of the
+##special matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
